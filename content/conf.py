44c6bb714d4fbdab933f66742e0477f0565cf280
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2025-03-19 17:33:31 +0200

    copyright to contributors

--- content/conf.py
+++ content/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = "Jupyter notebooks"
-copyright = "CodeRefinery team"
-author = "CodeRefinery team"
+copyright = "CodeRefinery contributors"
+author = "CodeRefinery contributors"
 github_user = "coderefinery"
 github_repo_name = "jupyter"  # auto-detected from dirname if blank
 github_version = "main"
